efdd66fd3f7f5b3676c0634f0a6bbdc866b64e4d
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2020-10-14 01:35:27 +0100

    Fixes

--- Assets/Scripts/InfinteTrack.cs
+++ Assets/Scripts/InfinteTrack.cs
@@ -7,9 +7,16 @@ public class InfinteTrack : MonoBehaviour
     public GameObject instance;
     public GameObject obstacle;
     private Vector3 _bounds;
+    private float _iNeedAVariable;
     private float _obstacleSpawnDistance;
-    private static float obstacles = 10;
+    private static float totalObstacles;
+    private float obstacles = 10;
     private bool testicles = true;
+
+    private void Start()
+    {
+        //totalObstacles = obstacles;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,20 +25,22 @@ public class InfinteTrack : MonoBehaviour
             var track = Instantiate(instance, new Vector3(0, 0, this.transform.position.z + (instance.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>().bounds.size.z * 15)), this.transform.rotation);
             testicles = false;
         }
-        _obstacleSpawnDistance = (instance.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>().bounds.size.z * 15) / obstacles;
+        //obstacles = totalObstacles;
+        _iNeedAVariable = instance.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>().bounds.size.z * 25;
+        _obstacleSpawnDistance = _iNeedAVariable / obstacles;
+        _bounds = this.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>().bounds.size;
         Obstacles();
     }
 
     private void Obstacles()
     {
-        _bounds = this.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Renderer>().bounds.size;
         //nya nya object pooling not arsed rn.
         for (int i = 0; i < obstacles; i++)
         {
-            Instantiate(obstacle, new Vector3(Random.Range(_bounds.x * 7, -_bounds.x * 7), Random.Range(_bounds.x * 5, -_bounds.x * 5), this.transform.position.z + (_obstacleSpawnDistance * obstacles)), this.transform.rotation);
+            Instantiate(obstacle, new Vector3(Random.Range(_bounds.x * 7, -_bounds.x * 7), Random.Range(_bounds.x * 5, -_bounds.x * 5), (this.transform.position.z -( _iNeedAVariable/2)) + (_obstacleSpawnDistance * obstacles)), this.transform.rotation);
             obstacles--;
         }
-        obstacles++;
+        //totalObstacles++;
         Destroy(this);
     }
 }
